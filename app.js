4f0b478185ea3b5062082a0cc83f06aa4fa679c9
otarim <otarim.com@gmail.com>
2013-01-24 23:04:42 +0800

    23:04

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var app = app || {};
 		defaults: function() {
 			return {
 				title: 'empty...',
-				order: todos.nextOrder();
-				done: false;
+				order: todos.nextOrder(),
+				done: false
 			}
-		}
+		},
 
 	// 初始化之后每一个todo都设定了默认标题
 		initialize: function() {
@@ -18,16 +18,16 @@ var app = app || {};
 					title: this.defaults().title
 				})
 			}
-		}
-	})
+		},
 
-	// 切换完成状态
+		// 切换完成状态
 		toggleDone: function() {
 			this.save({
 				done: !this.get('done')
 			})
-		} 
-
+		}
+	})
+	 
 	app.Collection = Backbone.Collection.extend({
 		
 		model: app.Model,
@@ -39,19 +39,19 @@ var app = app || {};
 				// todo == this 回调函数执行后返回真的元素将会被保留返回一个新的数组
 				return todo.get('done')
 			})
-		}
+		},
 
 		// 将this.done返回的数组(使用apply将数组分离为一个个的实参)从todolist中除去然后返回
 		remain: function() {
 			return this.without.apply(this,this.done())
-		}
+		},
 
 		//
 		nextOrder: function() {
 			if(!this.get('order')) return 1;
 			// 最后一个元素的order+1
 			else this.last().get('order') + 1;
-		}
+		},
 
 		// 根据order排序 通过pluck(key)返回key对应的value
 		comparator: function(todo) {
@@ -63,6 +63,25 @@ var app = app || {};
 	var todos = new app.Collection;
 
 	app.View = Backbone.View.extend({
+
+		tagName: 'li',
+
+		template: _.template($('#').html()),
+
+		events: {
+			'click ': 'toggleDone',
+			'dbclick ': 'edit',
+			'keypress ': 'update',
+			'click ': 'clear',
+			'blur': 'close'
+		},
+
+		initialize: function() {
+			// 监听change以及destroy事件
+			this.listenTo(this.model,'change',this.render);
+			this.listenTo(this.model,'destroy',this.remove);
+		} 
+
 
 	})
 
